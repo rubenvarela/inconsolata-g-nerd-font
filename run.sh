ff55b7ca0fc495a158c93562a0e5f882397813d7
Ruben Varela <ruben@local>
2020-03-30 01:35:49 -0400

    Initial commit.
    Script to generate inconsolata-g nerd font

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -L https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/font-patcher -o font-patcher
+chmod u+x ./font-patcher
+
+## I use git mainly. Installing git-svn then
+sudo apt install git-svn
+mkdir -p src/glyphs
+cd src/glyphs
+git svn init https://github.com/ryanoasis/nerd-fonts/trunk/src/glyphs
+git svn fetch
+cd ../../
+
+## if you want to use svn, this should work
+#sudo apt install subversion
+#mkdir src
+#cd src
+#svn checkout https://github.com/ryanoasis/nerd-fonts/trunk/src/glyphs
+#cd ../
+
+python3 ./font-patcher --progressbar -c inconsolata-g_font/Inconsolata-g.otf
